b2b9c292fed9c06dc6a6a8413f0a1056075b2631
wjdwnsdud95 <wjdwnsdud95@gmail.com>
2025-09-11 12:59:15 +0900

    250911 브런치 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>JunYeong</h1>
+      <h1>JunYeong green</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
