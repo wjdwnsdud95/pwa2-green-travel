aca3ad1dd333832014a24067cd255c0c34f42116
wjdwnsdud95 <wjdwnsdud95@gmail.com>
2025-09-12 14:29:21 +0900

    250912 Redux 기본 구조

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Router from './routes/Router.jsx';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </StrictMode>,
 )
