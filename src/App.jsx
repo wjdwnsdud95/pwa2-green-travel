dac3065432563e0918a39d14f83307bca6e75c62
wjdwnsdud95 <wjdwnsdud95@gmail.com>
2025-09-12 11:13:32 +0900

    250912 라우터 셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from './components/common/Header.jsx';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <main>
+        <Outlet />
+      </main>
     </>
   )
 }
 
-export default App
+export default App;
